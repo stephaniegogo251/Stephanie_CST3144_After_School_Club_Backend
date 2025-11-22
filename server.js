e910121e365417e323658b1f666f3398c5ef20d4
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2025-11-22 17:33:10 +0400

    updated logging format

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use ((req,res,next) => {
 });
 
 app.use(function(req, res, next){
-    console.log(`Request method: ${req.method}, path: ${req.path}, time: ${new Date()}`);
+    console.log(`${req.method} ${req.path} time: ${new Date()}`);
     next();
 });
  
