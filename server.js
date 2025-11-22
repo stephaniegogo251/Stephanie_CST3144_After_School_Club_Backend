df598e303a5e8836016a2c05093bdc8b236c0c59
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2025-11-22 21:05:33 +0400

    middleware update for static files

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(function(req, res, next){
 });
  
 app.use(express.json());
+app.use('/static', express.static('static'));
 
 app.use(function(req, res, next){
     var filePath = path.join(__dirname, "static", req.url);
