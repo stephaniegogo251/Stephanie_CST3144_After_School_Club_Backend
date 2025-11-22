e0c9ed30be395a09596fae8f5cdf901548936941
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2025-11-22 17:25:25 +0400

    added logging code

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use ((req,res,next) => {
 });
 
 app.use(function(req, res, next){
-    console.log("Request date " + new Date());
+    console.log(`Request method: ${req.method}, path: ${req.path}, time: ${new Date()}`);
     next();
 });
  
