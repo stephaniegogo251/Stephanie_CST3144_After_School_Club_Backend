c4cd69e1d845e91efc80a0794c4e3dd9d4a3553b
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2025-11-23 20:47:44 +0400

    debugging middleware for images

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next){
     var filePath = path.join(__dirname, "static", req.url);
     fs.stat(filePath, function(err, fileInfo){
         if (err){
-            next(err);
+            res.send("File not found!");
             return;
         }
         if(fileInfo.isFile())
