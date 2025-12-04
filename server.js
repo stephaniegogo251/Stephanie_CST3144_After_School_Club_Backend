0a5b990b8c4694f6fcb57c04d1e41a75d5db9d40
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2025-12-04 14:14:22 +0400

    updated logging

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use ((req,res,next) => {
 });
 
 app.use(function(req, res, next){
-    console.log(`${req.method} ${req.path} time: ${new Date()}`);
+    console.log(`${req.method} ${req.path} ${req.query} time: ${new Date()}`);
     next();
 });
  
